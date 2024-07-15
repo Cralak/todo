b67651020afadbb8fa4b9380bc4fc43e6e2c8f44
Ludwig <115760654+Cralak@users.noreply.github.com>
2024-07-15 04:01:08 +0200

    began to make login, register and logout

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php require_once 'tools.php'; ?>
+<?php require_once 'tools.php';
+
+session_start();
+
+if(session_status() === PHP_SESSION_NONE){
+    header("Location: login.php");
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -20,12 +28,18 @@
 <body id="body">
     <div id="main">
         <header>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(0, 255, 0)">
-                <path d="M120-80v-60h100v-30h-60v-60h60v-30H120v-60h120q17 0 28.5 11.5T280-280v40q0 17-11.5 28.5T240-200q17 0 28.5 11.5T280-160v40q0 17-11.5 28.5T240-80H120Zm0-280v-110q0-17 11.5-28.5T160-510h60v-30H120v-60h120q17 0 28.5 11.5T280-560v70q0 17-11.5 28.5T240-450h-60v30h100v60H120Zm60-280v-180h-60v-60h120v240h-60Zm180 440v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360Z" />
-            </svg>
             <div class="title">
-                <h3>Todoes</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgb(0, 255, 0)">
+                    <path d="M120-80v-60h100v-30h-60v-60h60v-30H120v-60h120q17 0 28.5 11.5T280-280v40q0 17-11.5 28.5T240-200q17 0 28.5 11.5T280-160v40q0 17-11.5 28.5T240-80H120Zm0-280v-110q0-17 11.5-28.5T160-510h60v-30H120v-60h120q17 0 28.5 11.5T280-560v70q0 17-11.5 28.5T240-450h-60v30h100v60H120Zm60-280v-180h-60v-60h120v240h-60Zm180 440v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360Z" />
+                </svg>
+                <div class="title">
+                    <h3>Todoes</h3>
+                </div>
             </div>
+
+            <a href="logout.php" class="account">
+                Log out
+            </a>
         </header>
 
         <!-- ADD NEW TASK -->
